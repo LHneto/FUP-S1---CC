6b5d2ceadd5a35acf9e7945b9c802408b4493f78
Luihmn <61132305+LHneto@users.noreply.github.com>
2023-09-25 14:13:17 -0300

    questao16.c

--- Lista - FUP/questao16.c
+++ Lista - FUP/questao16.c
@@ -11,13 +11,15 @@ int main() {
     fatiasQueijo = 2 * quantidadeSanduiches; 
     fatiasPresunto = quantidadeSanduiches; 
 
-
+    int pesoQueijo = 100 * quantidadeSanduiches;
     int pesoCarne = 100 * quantidadeSanduiches; 
-
+    int pesoPresunto = 50 * quantidadeSanduiches;
+    
     
     printf("Quantidade de queijo necessária: %d fatias\n", fatiasQueijo);
     printf("Quantidade de presunto necessária: %d fatias\n", fatiasPresunto);
     printf("Quantidade de carne (hambúrguer) necessária: %d gramas\n", pesoCarne);
-
+    printf("Quantidade de presunto necessária: %d gramas\n", pesoPresunto);
+    printf("Quantidade de queijo necessária: %d gramas\n", pesoQueijo);
     return 0;
 }
