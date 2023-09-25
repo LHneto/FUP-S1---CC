407e4b5bd72b466a8ae2968305cd6c26215ce34c
Luihmn <61132305+LHneto@users.noreply.github.com>
2023-09-25 10:37:13 -0300

    questao1.c

--- Lista - FUP/questao1.c
+++ Lista - FUP/questao1.c
@@ -4,12 +4,12 @@ int main() {
     int A = 10;
     int B = 20;
 
-    // Troca os valores de A e B sem variável temporária
+
     A = A + B;
     B = A - B;
     A = A - B;
 
-    // Exibe os valores após a troca
+   
     printf("A: %d\n", A);
     printf("B: %d\n", B);
 
