1680d14012e15fc44f935b14ac683b7213070fed
Luihmn <61132305+LHneto@users.noreply.github.com>
2023-09-25 21:52:14 -0300

    questao16.c

--- Lista - FUP/questao16.c
+++ Lista - FUP/questao16.c
@@ -5,12 +5,13 @@ int main() {
     int peso_queijo = 50;       
     int peso_presunto = 50;     
     int peso_hamburguer = 100;  
+
     int quantidade_sanduiches;
 
+    
     printf("Informe a quantidade de sanduíches a fazer: ");
     scanf("%d", &quantidade_sanduiches);
 
-   
     printf("Quantidade de queijo necessária: %.2lf kg\n", (2.0 * peso_queijo * quantidade_sanduiches) / 1000.0);
     printf("Quantidade de presunto necessária: %.2lf kg\n", (peso_presunto * quantidade_sanduiches) / 1000.0);
     printf("Quantidade de carne (hambúrguer) necessária: %.2lf kg\n", (peso_hamburguer * quantidade_sanduiches) / 1000.0);
